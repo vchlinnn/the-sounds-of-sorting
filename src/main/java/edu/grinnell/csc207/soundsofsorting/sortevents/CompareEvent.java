6b1efe099f3d5edcf78a3cd3fb50fe955d0374d7
vchlinnn <chuclinh190305@gmail.com>
2025-03-21 22:09:10 -0500

    Add the events

--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CompareEvent.java
@@ -1,9 +1,31 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.Arrays;
+import java.util.List;
+
+
 /**
  * A <code>CompareEvent</code> logs a comparison a sort makes between two
  * indices in the array.
  */
-public class CompareEvent<T> {
-    // TODO: implement me!
+public class CompareEvent<T> implements SortEvent<T> {
+    private int i;
+    private int j;
+
+    public CompareEvent(int i, int j) {
+        this.i = i;
+        this.j = j;
+    }
+
+    public void apply(T[] arr) {
+
+    }
+
+    public List<Integer> getAffectedIndices() {
+        return Arrays.asList(i, j);
+    }
+
+    public boolean isEmphasized() {
+        return false;
+    }
 }
--- src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
+++ src/main/java/edu/grinnell/csc207/soundsofsorting/sortevents/CopyEvent.java
@@ -1,8 +1,29 @@
 package edu.grinnell.csc207.soundsofsorting.sortevents;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * A <code>CopyEvent</code> logs a copy of a value into an index of the array.
  */
-public class CopyEvent<T> {
-    // TODO: implement me!
+public class CopyEvent<T> implements SortEvent<T> {
+    private int index;
+    private T value;
+
+    public CopyEvent(int index, T value) {
+        this.index = index;
+        this.value = value;
+    }
+
+    public void apply(T[] arr) {
+       arr[index] = value;
+    }
+
+    public List<Integer> getAffectedIndices() {
+        return Arrays.asList(index);
+    }
+
+    public boolean isEmphasized() {
+        return true;
+    }
 }
